ac0f25f487bb1eaadd065a2543c19cad22241472
ivanbabkin <37915085+ivababkin@users.noreply.github.com>
2019-05-03 19:38:25 +0300

    Задача 1
    Реализован алгоритм Ахо-Корасик.
    Источники: 
    https://ru.m.wikipedia.org/wiki/Алгоритм_Ахо_—_Корасик
    https://m.habr.com/ru/post/198682/

--- main.cpp
+++ main.cpp
@@ -1,119 +1,149 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
-#include <set>
-
 using namespace std;
+const int k = 26,NMAX = 10000;
 
+struct bohrVrtx{
+    int nextVrtx[k];
+    int patNum;
+    int suffLink;
+    int autoMove[k];
+    int par;
+    int suffFlink;
+    bool flag;
+    char symb;
+};
 
+class BohrClass{
+public:
+    vector<bohrVrtx> bohr;
+    vector<string> pattern;
+    vector<int> vectOfSymb;//вектор позиций * в изначальной строке
+    BohrClass();
+    bohrVrtx makeBohrVrtx(int p,char c);
+    void addStringToBohr(const string& s);
+    bool isStringInBohr(const string& s);
+    int getAutoMove(int v, char ch);
+    int getSuffLink(int v);
+    int getSuffFlink(int v);
+    void check(int v,int i, string s);
+    void findAllPos(const string& s);
+    void printRes(string s);
 
-int turnCount(vector<int> mass)
-{
-    int res = 0;
-    for (int i = 0; i < 9; i++)
-    {
-        if (mass[i] != 0)
-            res++;
-    }
-    return res;
+};
+
+bohrVrtx BohrClass::makeBohrVrtx(int p,char c){
+    bohrVrtx v;
+    memset(v.nextVrtx, 255, sizeof(v.nextVrtx));
+    memset(v.autoMove, 255, sizeof(v.autoMove));
+    v.flag = false;
+    v.suffLink = -1;
+    v.par = p;
+    v.symb = c;
+    v.suffFlink = -1;
+    return v;
 }
 
-bool isGameEnded(vector<int> mass)
+BohrClass::BohrClass()
 {
-    if (turnCount(mass) == 9)
-        return true;
-    for (int i = 0; i < 3; i++)
-    {
-        if ((mass[i] != 0) && (mass[i] == mass[i + 3]) && (mass[i] == mass[i + 6]))
-        {
-            return true;
+    bohr.push_back(makeBohrVrtx(0,'$'));
+}
+
+void BohrClass::addStringToBohr(const string& s){
+    int num = 0;
+    for (int i = 0; i < s.length(); i++){
+        char ch=s[i] - 'a';
+        if (bohr[num].nextVrtx[ch] == -1){
+            bohr.push_back(makeBohrVrtx(num,ch));
+            bohr[num].nextVrtx[ch]=bohr.size() - 1;
         }
-        if ((mass[i * 3] != 0) && (mass[i * 3] == mass[i * 3 + 1]) && (mass[i * 3] == mass[i * 3 + 2]))
+        num=bohr[num].nextVrtx[ch];
+    }
+    bohr[num].flag=true;
+    pattern.push_back(s);
+    bohr[num].patNum=pattern.size()-1;
+}
+bool BohrClass::isStringInBohr(const string& s){
+    int num = 0;
+    for (int i = 0; i < s.length(); i++){
+        char ch = s[i] - 'a';
+        if (bohr[num].nextVrtx[ch] == -1){
+            return false;
+        }
+        num=bohr[num].nextVrtx[ch];
+    }
+    return true;
+}
+int BohrClass::getSuffLink(int v){
+    if (bohr[v].suffLink == -1)
+        if (v == 0 || bohr[v].par == 0)
+            bohr[v].suffLink = 0;
+        else
+            bohr[v].suffLink = getAutoMove(getSuffLink(bohr[v].par), bohr[v].symb);
+    return bohr[v].suffLink;
+}
+int BohrClass::getAutoMove(int v, char ch){
+    if (bohr[v].autoMove[ch] == -1)
+        if (bohr[v].nextVrtx[ch] != -1)
+            bohr[v].autoMove[ch] = bohr[v].nextVrtx[ch];
+        else
+            if (v == 0)
+                bohr[v].autoMove[ch] = 0;
+            else
+                bohr[v].autoMove[ch] = getAutoMove(getSuffLink(v), ch);
+    return bohr[v].autoMove[ch];
+}
+int BohrClass::getSuffFlink(int v){
+    if (bohr[v].suffFlink == -1){
+        int u=getSuffLink(v);
+        if (u == 0)
+            bohr[v].suffFlink = 0;
+        else
+            bohr[v].suffFlink = (bohr[u].flag) ? u : getSuffFlink(u);
+    }
+    return bohr[v].suffFlink;
+}
+void BohrClass::check(int v,int i, string s)
+{
+    for(int u = v;u != 0;u = getSuffFlink(u))
+    {
+        if (bohr[u].flag)
         {
-            return true;
+           // cout << i - pattern[bohr[u].patNum].length() + 1 << " " << pattern[bohr[u].patNum] << endl;
+            vectOfSymb.push_back((i - pattern[bohr[u].patNum].length()) +  pattern[bohr[u].patNum].length());
+            cout << (i - pattern[bohr[u].patNum].length() + 1) +  pattern[bohr[u].patNum].length() << endl;
         }
     }
-    if ((mass[0] != 0) && (mass[0] == mass[4]) && (mass[0] == mass[8]))
-    {
-        return true;
-    }
-    if ((mass[2] != 0) && (mass[2] == mass[4]) && (mass[2] == mass[6]))
-    {
-        return true;
-    }
-    return false;
+
 }
-
-void print(vector<int> mass)
+void BohrClass::printRes(string s)
 {
-    for (int i = 0; i < 9; i++)
+    int additional = 0;
+    for(int count = 0; count < vectOfSymb.size(); count++)
     {
-        cout << mass[i] << " ";
-        if((i == 2) || (i == 5) || (i == 8))
-            cout << endl;
+        s = s.insert(vectOfSymb[count] + additional, 1, '*');
+        additional++;
     }
+    cout << s << endl;
 }
-
-int whoIsNext(vector<int> mass)
-{
-    int res = 0;
-    for (int i = 0; i < 9; i++)
+void BohrClass::findAllPos(const string& s){
+    int u = 0;
+    for(int i = 0;i < s.length();i++)
     {
-        if (mass[i] != 0)
-            res++;
+        u=getAutoMove(u,s[i] - 'a');
+        check(u,i + 1, s);
     }
-    if (res % 2 == 0)
-        return 1;
-    else
-        return 2;
-    return 0;
+    printRes(s);
 }
-
-set <vector<int>> *generateSetOfNextTurns(vector<int> mass)
-{
-    int turn = whoIsNext(mass);
-
-    set <vector<int>> * setRes = new set <vector<int>>();
-    if (isGameEnded(mass))
-    {
-        return setRes;
-    }
-    for (int i = 0; i < 9; i++)
-    {
-        if (mass[i] == 0)
-        {
-            mass[i] = turn;
-            setRes->insert(mass);
-            mass[i] = 0;
-        }
-    }
-    return setRes;
-}
-
-void calculate(vector<int> mass, set <vector<int>>* allSet)
-{
-    set <vector<int>> *setTmp = generateSetOfNextTurns(mass);
-    for(auto i = setTmp->begin(); i != setTmp->end(); i++)
-    {
-        //if (isGameEnded(*i))
-            if (turnCount(*i) <= 9)
-                allSet->insert(*i);
-        calculate(*i, allSet);
-    }
-}
-
-int main() // 1=X  2=O
-{
-    vector<int> mass = {0, 0, 0,    0, 0, 0,    0, 0, 0};
-
-    set <vector<int>> * setMain = new set <vector<int>>();
-    calculate(mass, setMain);
-
-    for(auto i : *setMain)
-    {
-        print(i);
-        cout << "---" << endl;
-    }
-    cout << "size " << setMain->size() << endl;
-    cout << "Hello World!" << endl;
-    return 0;
+int main(){
+    //string s = "---";
+    //cout << s.insert( 2, 1, 'a' );
+    BohrClass * b = new BohrClass();
+    b->addStringToBohr("abc");
+    b->addStringToBohr("bcdc");
+    b->addStringToBohr("cccb");
+    b->addStringToBohr("bcdd");
+    b->addStringToBohr("bbbc");
+    b->findAllPos("abcdcbcddbbbcccbbbcccbb");//abc*dc*bcdd*bbbc*ccb*bbcccb*b
 }
